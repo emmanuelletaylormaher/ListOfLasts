ae32f2ef322ffba6fd311e8e2b33943f763dd0df
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2019-04-03 17:21:10 -0500

    test commit

--- beanHook.php
+++ beanHook.php
@@ -2,7 +2,7 @@
 
 require 'rb-mysql.php';
 R::setup('mysql:host=localhost;port=8889;
-	dbname=ListOfLasts', 'root', 'root');
+	dbname=ListOfLasts', 'test', 'root');
 
 
 R::freeze( FALSE ); //until production
